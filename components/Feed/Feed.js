24b6ac5af1a28f1d89489255b6174fa05bf29995
Mahedimm <mahedi15-12649@diu.edu.bd>
2022-03-25 00:52:53 +0600

    sidebar

---
+++ components/Feed/Feed.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Feed = () => {
+    return (
+        <div className="text-white flex-1 border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
+            FEED
+        </div>
+    );
+};
+
+export default Feed;
